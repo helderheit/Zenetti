1b39992799249ba430f86af4d4d6e247c338952f
Gregor <gregor.donabauer@gmx.de>
2020-04-08 11:01:40 +0200

    Api for plain annotations. [INFO] yolo-coco folder too large, has to be added externally.

--- app/webapp/js/api/annotations.js
+++ app/webapp/js/api/annotations.js
@@ -16,3 +16,22 @@ function apiGetPlainAnnotations(itemId, onSuccess, onError){
       }
   });
 }
+
+function apiApplyObjectDetection(collectionId, itemId, onSuccess, onError){
+  // Get a collection from the api and hand it over to onSuccess Callback
+    $.ajax
+    ({
+      type: "GET",
+      url: API_URL_PREFIX+collectionId+"/"+itemId +"/detected_objects.json",
+      contentType : 'application/json',
+      async: true,
+      success: function(data){
+        onSuccess(data);
+      },
+      error: onError,
+      beforeSend: function (xhr) {
+          xhr.setRequestHeader ("Authorization", "Basic " +
+                                btoa(sessionStorage["token"] + ":" + ""));
+      }
+  });
+}
